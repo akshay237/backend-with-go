515b0719e8c1d6bbc1d489f422e44e2bdf218788
akshay237 <akshaybeniwal12@gmail.com>
2025-05-31 07:00:04 +0000

    dummy commit

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-echo "run databse migration"
+echo "load the env vars"
 source /app/app.env
+echo "run databse migration"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
